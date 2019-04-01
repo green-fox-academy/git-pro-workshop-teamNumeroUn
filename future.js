a390849ac567b9a8e2f38adf3b217cff8f98de42
Orsi Nagy <orsolya.n91@gmail.com>
2019-04-01 13:42:56 +0200

    test2

--- future.js
+++ future.js
@@ -1,9 +1,9 @@
 'use strict';
 
-function future (parameter1, parameter2, parameter3) {
+function future (parameter1, parameter2, parameter3, param4) {
   console.log(parameter, 'ALTERED BY C');
   console.log("balbalbalbalb");
   console.log("proba");
 }
 ////sup
-future("wazzup", "eeeeeeeey", "agaegg");
+future("wazzup", "eeeeeeeey", "agaegg", "stasgtga");
