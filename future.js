036def458efb05968b48614ad659497c6a8d4ed0
Orsi Nagy <orsolya.n91@gmail.com>
2019-04-01 13:22:07 +0200

    d5

--- future.js
+++ future.js
@@ -1,7 +1,8 @@
 'use strict';
 
-function future (parameter) {
+function future (parameter1, parameter2) {
   console.log(parameter, 'ALTERED BY C');
+  console.log("balbalbalbalb");
 }
 ////sup
-future("wazzup");
+future("wazzup", "eeeeeeeey");
