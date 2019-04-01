ca4a75deea09e6318cc35eb917ca118b39ec574a
Barnabas Kalasz <barnabas.kalasz@gmail.com>
2019-04-01 13:05:25 +0200

    c5

--- cheer.js
+++ cheer.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function cheer(lang){
-  return lang;
+function cheer(lang, c){
+  return lang,c;
 }
 
-console.log(cheer("JavaScript"));
+console.log(cheer("JavaScript", ' <--  styleguide says you should use apostrophe instead of the two line quotation mark'));
