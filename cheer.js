d63522988fbcba0a97523c1654919fbd9c3684c5
AttilaH <attila.habensusz@gmail.com>
2019-05-13 17:09:59 +0200

    Update cheer.js

--- cheer.js
+++ cheer.js
@@ -1,7 +1,12 @@
 'use strict';
 
+<<<<<<< HEAD
 function cheer(lang){
   return "Cheer for"+lang;
+=======
+function cheer(langs){
+  return langs;
+>>>>>>> attila-twoparam
 }
 
 console.log(cheer("Javascript"));
